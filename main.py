73f754a851f3fcb71467a5bf1edba65069a87824
marceloaviana <96154514+marceloaviana@users.noreply.github.com>
2023-11-08 16:28:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Exercicio 21
+
+
+notas = {
+  "português": "10",
+  "matemática": "6",
+  "física": "7",
+  "história": "8",
+  "geografia": "9",
+  "biologia": "5"
+}
+
+materia = input("Digite o nome da matéria: ")
+
+if materia in notas:
+  nota = notas[materia]
+  print(f"A nota de {materia} é {nota}.")
+else:
+  print("Matéria não encontrada!")
